f6058bc7b618fb5349a7b70036283144b3128e3d
Hanna Kazello <hanna.kazella@instinctools.ru>
2018-03-28 17:58:24 +0300

    use vector in catalog

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -3,20 +3,29 @@
 import React, { PureComponent } from 'react';
 import { Link } from 'react-router-dom';
 
+import { Vector } from './ui/Grid';
+import Card from './Card';
+
 class Catalog extends PureComponent {
   render() {
     const { books } = this.props;
 
     return (
-      <div>
-        Hello Catalog
-        <span>{books[0].belmarcId}</span>
-        <Link
-          to="/"
-        >
-          Home
-        </Link>
-      </div>
+      <Vector justify="space-around">
+        <Vector direction="column">
+          Hello Catalog
+          <div>{books[0].belmarcId}</div>
+          <Link
+            to="/"
+          >
+            Home
+          </Link>
+        </Vector>
+        <Vector direction="column">
+          <Card book={books[0]} />
+          <Card book={books[1]} />
+        </Vector>
+      </Vector>
     );
   }
 }
