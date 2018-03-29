9c97e5f07422018e88d61d16c34d7f8e8a36bde5
HannaKazello <hanna.kazello@gmail.com>
2018-03-29 20:48:59 +0300

    add styled components for card

--- src/components/ui/Card.js
+++ src/components/ui/Card.js
@@ -5,37 +5,54 @@ import styled from 'styled-components';
 export const Card = styled.div.withConfig({
   displayName: 'Card',
 })`
-  background-color: rgb(207, 242, 230);
+  background-color: #d1c4e9;
 
   display: flex;
   justify-content: space-between;
   align-items: center;
 
-  margin: 1%;
-  padding: 1%;
-  min-width: 60px;
+  padding: 3%;
+  box-sizing: border-box;
+  max-width: 320px;
   width: 80%;
   height: 200px;
+  margin: 1rem;
 
-  border-radius: 3px;
-  border-color: rgb(158, 232, 186);
-  > img {
-    min-width: 100px;
-    min-height: 100px;
+  border-radius: 2px;
+
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+
+  &: hover {
+    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
   }
 `;
 
-export const Header = styled.div.withConfig({
+export const Title = styled.div.withConfig({
   displayName: 'Header',
 })`
-width: 100%;
-height: 100%;
-display: flex;
-box-sizing: border-box;
-> span {
-  white-space: nowrap;
+  margin-bottom: .125em;
+  font-size: 1.5em;
+  font-weight: 700;
+  line-height: 1.1;
+`;
+
+export const About = styled.p.withConfig({
+  displayName: 'Header',
+})`
+  line-height: 1.4;
   overflow: hidden;
-  min-width: 60px;
   text-overflow: ellipsis;
-}
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+`;
+
+export const Author = styled.div.withConfig({
+  displayName: 'Header',
+})`
+  color: #666;
+  font-size: .825em;
+  line-height: 1.4;
+  margin-bottom: .5em;
 `;
