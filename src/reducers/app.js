02cfc28e8ef93cde8dec6666f53e055ffb386d86
Hanna Kazello <hanna.kazella@instinctools.ru>
2018-04-20 17:03:30 +0300

    add reducer for auth [2]

--- src/reducers/app.js
+++ src/reducers/app.js
@@ -7,14 +7,20 @@ import type { $ExtractState } from './$ExtractState';
 
 import time from './time';
 import books, * as booksSelectors from './books';
+import auth, * as authSelectors from './auth';
 
 const reducers = {
   router,
   time,
   books,
+  auth,
 };
 
 export type AppState = $ExtractState<typeof reducers>;
 export default combineReducers(reducers);
 
 export const getBooks = (state: AppState): string => booksSelectors.getBooks(state.books);
+export const getAuthenticationStatus = (state: AppState) =>
+  authSelectors.getAuthenticationStatus(state.auth);
+export const getAuthenticationError = (state: AppState) =>
+  authSelectors.getAuthenticationError(state.auth);
