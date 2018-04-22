c77dc41c1b8d7836d17c200c82e41bde6778ecea
HannaKazello <hanna.kazello@gmail.com>
2018-04-22 20:21:02 +0300

    use searchkit layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,25 @@
 // @flow
 
 import React, { PureComponent } from 'react';
-import { SearchBox, Hits, MenuFilter } from 'searchkit';
+import {
+  SearchBox,
+  Hits,
+  MenuFilter,
+  ItemList,
+  Layout,
+  TopBar,
+  LayoutBody,
+  SideBar,
+  HierarchicalMenuFilter,
+  RefinementListFilter,
+  LayoutResults,
+  ActionBar,
+  ActionBarRow,
+  SelectedFilters,
+  ResetFilters,
+  NoHits,
+  HitsStats,
+} from 'searchkit';
 import Catalog from './Catalog';
 import { SearchBox as Header } from './ui/Search';
 import { FilterBox } from './ui/Filter';
@@ -14,29 +32,62 @@ const sourceFilter = [
   'refernces.value',
 ];
 
-class Home extends PureComponent {
-  render() {
-    return (
-      <Screen>
-        <Header>
-          <SearchBox />
-        </Header>
-        <Hits
-          hitsPerPage={50}
-          sourceFilter={sourceFilter}
-          listComponent={Catalog}
+const Home = () =>
+  (
+    <Layout>
+      <TopBar>
+        <SearchBox
+          autofocus
+          searchOnChange
         />
-        <FilterBox>
+      </TopBar>
+      <LayoutBody>
+        <SideBar>
+          <HierarchicalMenuFilter
+            fields={['fields.tag.raw', 'fields.value']}
+            fieldOptions={{ type: 'nested', options: { path: 'fields' } }}
+            title="Categories"
+            id="categories"
+          />
+          <RefinementListFilter
+            id="references"
+            title="References"
+            field="references.tag.raw"
+            fieldOptions={{ type: 'nested', options: { path: 'fields' } }}
+            operator="AND"
+            size={10}
+          />
           <MenuFilter
             field="fields.tag.raw"
             fieldOptions={{ type: 'nested', options: { path: 'fields' } }}
             title="Tags"
             id="tags"
+            listComponent={ItemList}
           />
-        </FilterBox>
-      </Screen>
-    );
-  }
-}
+        </SideBar>
+        <LayoutResults>
+          <ActionBar>
+
+            <ActionBarRow>
+              <HitsStats />
+            </ActionBarRow>
+
+            <ActionBarRow>
+              <SelectedFilters />
+              <ResetFilters />
+            </ActionBarRow>
+
+          </ActionBar>
+          <Hits
+            mod="sk-hits-glistrid"
+            hitsPerPage={50}
+            sourceFilter={sourceFilter}
+            listComponent={Catalog}
+          />
+          <NoHits />
+        </LayoutResults>
+      </LayoutBody>
+    </Layout>
+  );
 
 export default Home;
