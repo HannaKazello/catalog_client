9812d365dbeef3e78a968ee854e2f91ef9c0ed12
HannaKazello <hanna.kazello@gmail.com>
2018-06-17 04:03:23 +0300

    add load bucket action

--- src/actions/search.js
+++ src/actions/search.js
@@ -1,15 +1,22 @@
 // @flow
 
+type Buckets = Array<{ key: string, values: Array<Object> }>;
 export type Action =
   | { type: 'SEARCH', payload: { searchString: string } }
   | { type: 'RECORDS_LOADED', payload: { books: Array<Object> }}
+  | { type: 'LOAD_BUCKETS', payload: { keys: Buckets }}
 
 export const authenticate = (searchString: string): Action => ({
   type: 'SEARCH',
   payload: { searchString },
 });
 
-export const authenticationFailure = (books: Array<Object>): Action => ({
+export const loadRecords = (books: Array<Object>): Action => ({
   type: 'RECORDS_LOADED',
   payload: books,
+});
+
+export const loadBuckets = (buckets: Buckets): Action => ({
+  type: 'LOAD_BUCKETS',
+  payload: buckets,
 });
